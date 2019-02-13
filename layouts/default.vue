37d964c7e1855de394140f42726f30adddf4cf87
cherogit <chernenkoan91@gmail.com>
2019-02-14 00:09:02 +0300

    upload

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+div
+  nuxt/
+
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {}
+  }
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+
+div
+  h2 Добро пожаловать на главную
+  v-example-component
+
+</template>
+
+<script lang="ts">
+import { Vue, Component } from '~/modules/nuxt'
+import VExampleComponent from '~/components/example/component.vue'
+import { AppContext } from '~/typings/context'
+
+@Component({
+  components: { VExampleComponent }
+})
+export class VIndexPage extends Vue {
+
+}
+
+export default VIndexPage
+</script>
